a971e9db850b981905ea31ac6324efd11e3f7593
Kang Ken <Ken.Kang@KONE.com>
2020-05-28 13:44:49 +0800

    Multi-Attachment Database Load

--- dialogs/cn_dialogAdmin01.js
+++ dialogs/cn_dialogAdmin01.js
@@ -14,6 +14,7 @@ const {
 const { CardFactory, TurnContext } = require('botbuilder');
 const Hint = require('../config/cn_hint.json');
 const Menu = require('../config/cn_menu.json');
+const Card = require('../config/cn_card.json');
 const Database = require('../config/cn_database.json');
 const path = require('path');
 const request = require('request');
@@ -21,6 +22,7 @@ const csv = require('csv');
 const low = require('lowdb');
 const FileSync = require('lowdb/adapters/FileSync');
 var _ = require('lodash');
+var ACData = require("adaptivecards-templating");
 
 const CN_DIALOG_ADMIN01 = 'CN_DIALOG_ADMIN01';//Tender&VO业务客服联系人
 const DIALOG_WATERFALL = 'DIALOG_WATERFALL';
@@ -53,11 +55,12 @@ class CN_DialogAdmin01 extends ComponentDialog {
 
     async loadDBStep(stepContext) {
         var lastrefreshuser = stepContext.context.activity.from.name;
-        var db = new Array();
-        request(stepContext.result[0].contentUrl)
+        _.forEach(stepContext.result,function(value){
+            var db = new Array();
+            request(value.contentUrl)
             .pipe(csv.parse({ delimiter: ',', ltrim: true, from: 2 }))
             .pipe(csv.transform(function (line) {
-                switch (stepContext.result[0].id) {
+                switch (value.id) {
                     case 'Contact01': //Tender/VO 业务联系人
                     case 'Contact02': //Order 业务联系人
                         var regionindex = _.findIndex(db, { region: line[0] });
@@ -124,7 +127,7 @@ class CN_DialogAdmin01 extends ComponentDialog {
             .on('finish', function () {
                 try {
                     var adapter = new FileSync(
-                        path.resolve(__dirname, "../db/" + stepContext.result[0].db));
+                        path.resolve(__dirname, "../db/" + value.db));
                     var lowdb = low(adapter);
                     var d = new Date();
                     lowdb.defaults({ db: [], lastRefresh: {}, countRefresh: 0 })
@@ -145,20 +148,50 @@ class CN_DialogAdmin01 extends ComponentDialog {
                     console.log(e);
                 }
             });
+        }
+        );
+        
+        
         await stepContext.context.sendActivity(Hint.messageLoadDBSuccess);
         return await stepContext.endDialog({ index: 2 });
     }
 
     async dbPromptValidator(promptContext) {
-        if (promptContext.recognized.succeeded && promptContext.recognized.value[0].contentType === "text/csv") {
-            var dbinfo = _.find(Database, { 'csv': promptContext.recognized.value[0].name });
-            if (dbinfo) {
-                promptContext.recognized.value[0].db = dbinfo.db;
-                promptContext.recognized.value[0].id = dbinfo.id;
+        if (promptContext.recognized.succeeded) {
+            var message = "";
+            _.forEach(promptContext.recognized.value, function (value) {
+                var dbinfo = _.find(Database, { 'csv': value.name });
+                if (dbinfo) {
+                    value.db = dbinfo.db;
+                    value.id = dbinfo.id;
+                    value.delete = false;
+                    //_.concat(message,value.name + Hint.messageCorrectFileName);
+                    message+=value.name + Hint.messageCorrectFileName;
+                }
+                else {
+                    value.delete = true;
+                    //_.concat(message,value.name + Hint.messageIncorrectFileName);
+                    message += value.name + Hint.messageIncorrectFileName;
+                }
+                
+            });
+            //var template = new ACData.Template(Card.Admin01_AdaptiveAttachment);
+            //var card = template.expand({ $root:message});
+            //await promptContext.context.sendActivity(
+            //    {
+            //        attachments: [CardFactory.adaptiveCard(card)]
+            //    });
+            await promptContext.context.sendActivity(message);
+            
+            _.remove(promptContext.recognized.value,function(o){
+                    return o.delete;
+                });
+            if(_.size(promptContext.recognized.value)!==0)
+            {
                 return true;
             }
-            else {
-                await promptContext.context.sendActivity(Hint.messageIncorrectFileName);
+            else{
+                await promptContext.context.sendActivity(Hint.messageLoadDBRetry) ;
             }
         }
 
