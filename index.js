e3247056f846ed4d03f3989c66d569953471b28c
Kang Ken <Ken.Kang@KONE.com>
2020-06-11 11:08:36 +0800

    Bot Switch

--- index.js
+++ index.js
@@ -6,15 +6,16 @@ const restify = require('restify');
 const log4js = require('log4js');
 const logconf = require('./config/cn_log.json');
 log4js.configure(logconf);
+const Hint = require('./config/cn_hint.json');
 const cn_logger = log4js.getLogger();
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { 
-    BotFrameworkAdapter, 
-    MemoryStorage, 
-    UserState, 
-    ConversationState 
+const {
+    BotFrameworkAdapter,
+    MemoryStorage,
+    UserState,
+    ConversationState
 } = require('botbuilder');
 
 const { CN_BotSES } = require('./bots/cn_botSES');
@@ -26,8 +27,8 @@ require('dotenv').config({ path: ENV_FILE });
 
 // Create HTTP server
 const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3977, function() {
-    cn_logger.debug(`${ server.name } listening to ${ server.url }`);
+server.listen(process.env.port || process.env.PORT || 3977, function () {
+    cn_logger.debug(`${server.name} listening to ${server.url}`);
 });
 
 // Create adapter.
@@ -46,15 +47,15 @@ const cn_userState = new UserState(cn_memoryStorage);
 const cn_conversationState = new ConversationState(cn_memoryStorage);
 
 // Create the main dialog.
-const cn_dialogRoot = new CN_DialogRoot(cn_userState,cn_logger);
-const cn_botSES = new CN_BotSES(cn_conversationState, cn_userState, cn_dialogRoot,cn_logger);
+const cn_dialogRoot = new CN_DialogRoot(cn_userState, cn_logger);
+const cn_botSES = new CN_BotSES(cn_conversationState, cn_userState, cn_dialogRoot, cn_logger);
 
 // Catch-all for errors.
 cn_adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
     //       application insights.
-    cn_logger.error(`${context.activity.from.name}[onTurnError] unhandled error: ${ error }`);
+    cn_logger.error(`${context.activity.from.name}[onTurnError] unhandled error: ${error}`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     //await context.sendTraceActivity(
@@ -65,24 +66,28 @@ cn_adapter.onTurnError = async (context, error) => {
     //);
 
     // Send a message to the user
-    await context.sendActivity('The bot encountered an error or bug.');
-    await context.sendActivity('To continue to run this bot, please fix the bot source code.');
+    //await context.sendActivity('The bot encountered an error or bug.');
+    await context.sendActivity(Hint.error);
     // Clear out state
     await cn_conversationState.clear(context);
     await cn_conversationState.saveChanges(context, false);
 };
 
 // Listen for incoming requests. 
-server.post('/api/cn', (req, res) => {
-    cn_adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
-        await cn_botSES.run(context);
+if (process.env.CN_BOT === "ON") {
+    server.post('/api/cn', (req, res) => {
+        cn_adapter.processActivity(req, res, async (context) => {
+            // Route to main dialog.
+            await cn_botSES.run(context);
+        });
     });
-});
+}
 
-server.get('/', (req, res,next) => {
+
+server.get('/', (req, res, next) => {
     var d = new Date();
-    res.send('The site is running, today a is: '+ d.toLocaleDateString() + ' time is:' + d.toLocaleTimeString());
+    res.send('The '+ process.env.NODE_ENV +' site is running, today is: ' + d.toLocaleDateString() + ' time is:' + d.toLocaleTimeString());
     return next();
 
 });
+
