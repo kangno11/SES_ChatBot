be8f2d7a48d8ce2759a680e14fe7f2703e497423
Kang Ken <Ken.Kang@KONE.com>
2020-06-11 15:31:45 +0800

    Add Statistics

--- dialogs/cn_dialogRoot.js
+++ dialogs/cn_dialogRoot.js
@@ -10,6 +10,10 @@ const {
     ChoiceFactory
 } = require('botbuilder-dialogs');
 var _ = require('lodash');
+const Database = require('../config/cn_database.json');
+const path = require('path');
+const low = require('lowdb');
+const FileSync = require('lowdb/adapters/FileSync');
 
 const { CN_DialogContact01,
     CN_DIALOG_CONTACT01 } = require('./cn_dialogContact01');
@@ -68,6 +72,63 @@ class CN_DialogRoot extends ComponentDialog {
 
         this.initialDialogId = DIALOG_WATERFALL;
     }
+
+    /** statistics for menu entry */
+    async countFeedback(menu, satisfied) {
+        var adapter = new FileSync(
+            path.resolve(__dirname, "../db/" + Database.Admin03.db));
+        var lowdb = low(adapter);
+        var d = new Date();
+        var mth = d.toLocaleDateString();
+        if (satisfied) {
+            lowdb.get(mth)
+                .get(menu)
+                .update('good', n => n + 1)
+                .write();
+        }
+        else{
+            lowdb.get(mth)
+            .get(menu)
+            .update('bad', n => n + 1)
+            .write();
+        }
+    }
+    async countMenuEntry(menu) {
+        var adapter = new FileSync(
+            path.resolve(__dirname, "../db/" + Database.Admin03.db));
+        var lowdb = low(adapter);
+        var d = new Date();
+        var mth = d.toLocaleDateString();
+
+
+
+        var mth_check = lowdb.get(mth).value();
+        if (mth_check) {
+            var menu_check = lowdb.get(mth).get(menu).value();
+            if (menu_check) {
+                lowdb.get(mth)
+                    .get(menu)
+                    .update('entry', n => n + 1)
+                    .write();
+            }
+            else {
+                lowdb.get(mth)
+                    .set(menu, { entry: 1, good: 0, bad: 0 })
+                    .write();
+
+            }
+        }
+        else {
+            var body = {};
+            _.set(body, menu, { entry: 1, good: 0, bad: 0 });
+            lowdb.set(mth, body)
+                .write();
+        }
+
+        //lowdb.update(menu+'_entry', n => n + 1)
+        //    .write();
+    }
+
 
     /**
      * The run method handles the incoming activity (in the form of a TurnContext) and passes it through the dialog system.
@@ -135,10 +196,16 @@ class CN_DialogRoot extends ComponentDialog {
             case 0: //1,联系人查询
                 switch (stepContext.values.subMenu) {
                     case 0://1, Tender&VO业务客服联系人
+                        stepContext.values.idMenu = "Contact01";
+                        await this.countMenuEntry('Contact01');
                         return await stepContext.beginDialog(CN_DIALOG_CONTACT01);
                     case 1://2, 排产业务客服联系人
+                        stepContext.values.idMenu = "Contact02";
+                        await this.countMenuEntry('Contact02');
                         return await stepContext.beginDialog(CN_DIALOG_CONTACT02);
                     case 2://3, 特殊流程联系人
+                        stepContext.values.idMenu = "Contact03";
+                        await this.countMenuEntry('Contact03');
                         return await stepContext.beginDialog(CN_DIALOG_CONTACT03);
 
                     case 3://4.返回上一级菜单
@@ -148,10 +215,16 @@ class CN_DialogRoot extends ComponentDialog {
             case 1://2,项目状态查询
                 switch (stepContext.values.subMenu) {
                     case 0://1.国内询价项目
+                        stepContext.values.idMenu = "Project01";
+                        await this.countMenuEntry('Project01');
                         return await stepContext.beginDialog(CN_DIALOG_PROJECT01);
                     case 1://2.国内排产项目
+                        stepContext.values.idMenu = "Project02";
+                        await this.countMenuEntry('Project02');
                         return await stepContext.beginDialog(CN_DIALOG_PROJECT02);
                     case 2://5.VO项目
+                        stepContext.values.idMenu = "Project03";
+                        await this.countMenuEntry('Project03');
                         return await stepContext.beginDialog(CN_DIALOG_PROJECT03);
                     case 3://6.返回上一级菜单
                         return await stepContext.replaceDialog(CN_DIALOG_ROOT);
@@ -178,9 +251,11 @@ class CN_DialogRoot extends ComponentDialog {
     async finalStep(stepContext) {
         switch (stepContext.result.index) {
             case 0: //满足期望
+                await this.countFeedback(stepContext.values.idMenu, true);
                 await stepContext.context.sendActivity(Hint.messageGoodFeedback);
                 break;
             case 1://不满足期望
+                await this.countFeedback(stepContext.values.idMenu, false);
                 await stepContext.context.sendActivity(Hint.messageBadFeedback);
                 break;
             case 2://管理员菜单，不做处理
